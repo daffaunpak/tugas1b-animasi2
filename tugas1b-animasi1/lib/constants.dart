b7dcca038de634b207a1ff3c647c35d0a86058f1
Daffa Adzhani <daffaunpak>
2022-10-31 10:26:46 +0700

    first commit

---
+++ tugas1b-animasi1/lib/constants.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const Color primaryColor = Color.fromARGB(255, 223, 12, 12);
+const Color bgColor = Color.fromARGB(255, 244, 244, 244);
+
+const double defaultPadding = 16.0;
+const double defaultBorderRadius = 12.0;
